e630558e3c576678bf9af3002bc33c347e5c786d
DMiTRONiK <dmitronik@gmail.com>
2022-05-14 11:47:06 +0000

    checkurl.sh

--- checkurl.sh
+++ checkurl.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
-code=$(curl -Is -o /dev/null -w "%{http_code}\n" --connect-timeout 5  http://$1:80/)
-
-echo "response code:$code"
-if [ "$code" == "200" ] ;
-then
-  echo "success"
-  exit 0;
-else
-  echo "error"
-  exit 1;
-fi
+c=0
+while ((c++ < 5 )); do
+  code=$(curl -Is -o /dev/null -w "%{http_code}\n" --connect-timeout 5  http://$1:80/)
+  echo "response code:$code"
+  if [ "$code" == "200" ] ;
+  then
+    echo "success"
+    STATUS=0
+    break
+  else
+    echo "error"
+    STATUS=1
+    sleep 10
+  fi
+done
+exit $STATUS

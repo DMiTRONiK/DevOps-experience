95bf942b8e0f85218f39826d24bdfd6e8a8656a3
DMiTRONiK <dmitronik@gmail.com>
2022-06-08 14:17:34 +0000

    user_data.sh

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh get-docker.sh
-sudo usermod -aG docker ubuntu
+sh get-docker.sh
+usermod -aG docker ubuntu
 echo added user to docker group
-sudo su - ubuntu
+#su - ubuntu
